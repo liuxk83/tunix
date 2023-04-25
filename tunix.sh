ee362f32af02e5b0c4be6ffabe8199e307fe57a6
Kevin Liu <liuxk@eniac.seas.upenn.edu>
2023-04-25 02:09:40 -0400

    Implement play functionality

--- tunix.sh
+++ tunix.sh
@@ -1,3 +1,22 @@
+function play_songs() {
+	paths="$(sed -r 's/^.* \(([0-9]+)\)/songs\/\1.mp3/g' <<< $1)"
+	read -p "Would you like to play these song(s)? ([y]es/[n]o/[s]huffle) " ans
+	while [ -n "$ans" ]; do
+		case "$ans" in
+			"y")
+				echo "$paths" | xargs mpv
+				;;
+			"s")
+				echo "$paths" | xargs mpv --shuffle
+				;;
+			*)
+				printf "Song(s) were not played.\n"
+				break
+		esac
+		read -p "Would you like to play these song(s) again? ([y]es/[n]o/[s]huffle) " ans
+	done
+}
+
 function play() {
 	printf "\n*** Search/Play Songs ***\n"
 	printf "To search songs by title and artist, use \"[title];[artist]\".\n"
@@ -10,27 +29,15 @@ function play() {
 	while [ -n "$title" ]; do
 		if [ -n "$artist" ]; then
 			result="$(grep -iE "^.*$title.* by .*$author.*" catalog.txt)"
-		elif [ "$title" == "*" ]; then
-			result="$(cat catalog.txt)"
 		else
 			result="$(grep -iE "^.* \(.*$title.*\)$" catalog.txt)"
 		fi
-		paths="$(sed -r 's/^.* \(([0-9]+)\)/songs\/\1.mp3/g' <<< $result)"
-		if [ -z "$paths" ]; then
+		if [ -z "$result" ]; then
 			printf "No results found for $title."
 		else
-			echo "$result"
-			read -p "Would you like to play these song(s)? ([y]es/[n]o/[s]huffle) " ans
-			case "$ans" in
-				"y")
-					mpv "$paths"
-					;;
-				"s")
-					mpv --shuffle "$paths"
-					;;
-				*)
-					printf "Song(s) were not played.\n"
-			esac
+			printf "$result"
+			printf "\n\n"
+			play_songs "$result"
 		fi
 		echo
 		read -p "What song(s) would you like to find? " title artist
@@ -47,7 +54,18 @@ function main_login() {
 }
 
 function main_nologin() {
-	printf "\n##################################################\n#                                                #\n#    Welcome to tunix, a Unix-based music        #\n#    player! You are currently not logged in.    #\n#    What would you like to do?                  #\n#                                                #\n#    ----------------------------------------    #\n#                                                #\n#    (1) Search/Play                             #\n#    (2) Register                                #\n#    (3) Log in                                  #\n#    (Enter) Quit                                #\n#                                                #\n##################################################\n\n"
+	printf "\n--------------------------------------------------\n"
+	printf "Welcome to tunix, a Unix-based music player!\n"
+	printf "You are currently not logged in.\n"
+	printf "What would you like to do?"
+	printf "\n--------------------------------------------------\n"
+	printf "(1) Search/Play\n"
+	printf "(2) Register\n"
+	printf "(3) Log in \n"
+	printf "(Enter) Quit"
+	printf "\n--------------------------------------------------\n"
+
+	#printf "\n##################################################\n#                                                #\n#    Welcome to tunix, a Unix-based music        #\n#    player! You are currently not logged in.    #\n#    What would you like to do?                  #\n#                                                #\n#    ----------------------------------------    #\n#                                                #\n#    (1) Search/Play                             #\n#    (2) Register                                #\n#    (3) Log in                                  #\n#    (Enter) Quit                                #\n#                                                #\n##################################################\n\n"
 	read option
 	while [ ! -z "$option" ]; do
 		case "$option" in
@@ -63,7 +81,16 @@ function main_nologin() {
 			*)
 				printf "Invalid option.\n"
 		esac
-		printf "\n##################################################\n#                                                #\n#    Welcome to tunix, a Unix-based music        #\n#    player! You are currently not logged in.    #\n#    What would you like to do?                  #\n#                                                #\n#    ----------------------------------------    #\n#                                                #\n#    (1) Search/Play                             #\n#    (2) Register                                #\n#    (3) Log in                                  #\n#    (Enter) Quit                                #\n#                                                #\n##################################################\n\n"
+		printf "\n--------------------------------------------------\n"
+        	printf "Welcome to tunix, a Unix-based music player!\n"
+        	printf "You are currently not logged in.\n"
+        	printf "What would you like to do?"
+        	printf "\n--------------------------------------------------\n"
+        	printf "(1) Search/Play\n"
+        	printf "(2) Register\n"
+        	printf "(3) Log in \n"
+        	printf "(Enter) Quit"
+        	printf "\n--------------------------------------------------\n"
 		read option
 	done
 	printf "Shutting down...\n"
